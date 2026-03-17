9f890ad927228e924b7006e825df1cf04db49b2f
Stefan Ninkov <stefanninkov@192.168.1.53>
2026-03-17 08:33:06 +0100

    Updates

--- assets/use-page-title-BCQiUGyy.js
+++
@@ -1,1 +0,0 @@
-import{m as o}from"./index-NLBXZMEQ.js";function u(e){o.useEffect(()=>{const t=document.title;return document.title=e?`${e} — Forge`:"Forge",()=>{document.title=t}},[e])}export{u};
---
+++ assets/use-page-title-Xd9dMC5b.js
@@ -0,0 +1,1 @@
+import{m as o}from"./index-xdfMgPut.js";function u(e){o.useEffect(()=>{const t=document.title;return document.title=e?`${e} — Forge`:"Forge",()=>{document.title=t}},[e])}export{u};
